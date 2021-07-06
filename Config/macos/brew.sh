6c79195d85b41715386ce75714f0be555d8790a7
Ricardo Cino <ricardo@cino.io>
2021-07-06 19:37:52 +0200

    Update brew

--- Config/macos/brew.sh
+++ Config/macos/brew.sh
@@ -8,7 +8,7 @@ echo "Installing brew..."
 
 if test ! $(which brew)
 then
-	## Don't prompt for confirmation when installing homebrew
+  ## Don't prompt for confirmation when installing homebrew
   /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)" < /dev/null
 fi
 
@@ -36,14 +36,24 @@ brew install fzf
 brew install gnupg
 brew install --cask gpg-suite
 
+# Various software for daily use
+brew install --cask iterm2
+brew install --cask microsoft-edge
+brew install --cask spotify
+brew install --cask insomnia
+brew install --cask sublime-text
+brew install --cask visual-studio-code
+brew install --cask notion
+brew install --cask tableplus
+brew install --cask mongodb-compass
+brew install --cask 1password
+brew install --cask ferdi
+brew install --cask raycast
+
 # Terraform
 brew tap hashicorp/tap
 brew install hashicorp/tap/terraform
 brew install hashicorp/tap/packer
-brew install graphviz
 
 # Golang
 brew install go
-
-# Raycast
-brew install --cask raycast
